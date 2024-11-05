04197e40ff2cd702932ec7e8721066b00356406d
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-11-05 12:33:49 +0000

    init SurvivalCustom Pt.5

--- examples/genboost.py
+++ examples/genboost.py
@@ -10,24 +10,18 @@ X = X.astype(float)
 
 estimator = ComponentwiseGenGradientBoostingSurvivalAnalysis(regr = RidgeCV(), loss="coxph")
 estimator2 = ComponentwiseGenGradientBoostingSurvivalAnalysis(regr = ExtraTreeRegressor(), loss="coxph")
-estimator3 = ComponentwiseGenGradientBoostingSurvivalAnalysis(regr = ExtraTreesRegressor(), 
-                                                              n_estimators=1, loss="coxph")
 
 estimator.fit(X, y)
 estimator2.fit(X, y)
-estimator3.fit(X, y)
 
 print(estimator)
 print(estimator2)
-print(estimator3)
 
 surv_funcs = estimator.predict_survival_function(X.iloc[:2])
 surv_funcs2 = estimator2.predict_survival_function(X.iloc[:2])
-surv_funcs3 = estimator3.predict_survival_function(X.iloc[:2])
 
 print(surv_funcs[1])
 print(surv_funcs2[1])
-print(surv_funcs3[1])
 
 for fn in surv_funcs:
     plt.step(fn.x, fn(fn.x), where="post")
@@ -35,11 +29,6 @@ for fn in surv_funcs:
     plt.show()
 
 for fn in surv_funcs2:
-    plt.step(fn.x, fn(fn.x), where="post")
-    plt.ylim(0, 1)
-    plt.show()
-
-for fn in surv_funcs3:
     plt.step(fn.x, fn(fn.x), where="post")
     plt.ylim(0, 1)
     plt.show()
