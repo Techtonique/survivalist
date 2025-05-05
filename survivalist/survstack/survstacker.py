1261d88d5043f1c3491956202c7bf3b56fae2a30
Thierry Moudiki <thierry.moudiki@gmail.com>
2025-05-05 14:30:06 +0200

    do type_sim Pt.1

--- survivalist/survstack/survstacker.py
+++ survivalist/survstack/survstacker.py
@@ -3,8 +3,12 @@ import numpy as np
 from ..base import SurvivalAnalysisMixin
 from sklearn.linear_model import LogisticRegression
 from sklearn.calibration import CalibratedClassifierCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 from .transformer import SurvivalStacker
 from ..util import check_array_survival
+from ..utils import simulate_replications
 from ..base import SurvivalAnalysisMixin
 from ..linear_model.coxph import BreslowEstimator
 from ..util import check_array_survival
@@ -23,8 +27,10 @@ class SurvStacker(SurvivalAnalysisMixin):
     """
     def __init__(
         self,
-        clf=LogisticRegression(),
+        clf=RandomForestClassifier(),
         loss="squared",
+        type_sim="none",
+        replications=250,
         random_state=42,
         **kwargs      
     ):
@@ -39,6 +45,18 @@ class SurvStacker(SurvivalAnalysisMixin):
         of Cox's proportional hazards model. The loss 'squared' minimizes a
         squared regression loss that ignores predictions beyond the time of censoring,
         and 'ipcwls' refers to inverse-probability of censoring weighted least squares error.
+
+        type_sim (str): Method for simulation:
+                      - 'none': No simulation. 
+                      - 'kde': Kernel Density Estimation.
+                      - 'bootstrap': Bootstrap resampling.                      
+                      - 'normal': Parametric distribution fitting.
+                      - 'ecdf': Empirical CDF-based sampling.
+                      - 'permutation': Permutation resampling.
+                      - 'smooth_bootstrap': Smoothed bootstrap with added noise.
+        
+        replications : int, default: 250
+            The number of replications for the simulation.
         
         random_state : int seed, RandomState instance, or None, default: None
             The seed of the pseudo random number generator to use when
@@ -52,14 +70,16 @@ class SurvStacker(SurvivalAnalysisMixin):
             self.clf.set_params(random_state=self.random_state)
         except Exception as e:
             pass 
-        self.clf = CalibratedClassifierCV(clf, 
-                                          **kwargs)        
-        self.ss = SurvivalStacker()
-        self.times_ = None
-        self.unique_times_ = None
+        self.clf = CalibratedClassifierCV(clf, **kwargs)        
+        self.ss = SurvivalStacker()        
         self._baseline_model = None
         self.loss = loss 
         self._loss = LOSS_FUNCTIONS[self.loss]()
+        self.replications = replications 
+        self.times_ = None
+        self.unique_times_ = None
+        self.calibrated_residuals_ = None 
+
     
     def _get_baseline_model(self):
         """
@@ -72,6 +92,7 @@ class SurvStacker(SurvivalAnalysisMixin):
         """
         return self._baseline_model
 
+
     def _set_baseline_model(self, X, event, time):
         if isinstance(self._loss, CoxPH):
             risk_scores = self.predict(X)
@@ -80,6 +101,7 @@ class SurvStacker(SurvivalAnalysisMixin):
             )
         else:
             self._baseline_model = None
+
 
     def fit(self, X, y, **kwargs):
         """
@@ -108,15 +130,32 @@ class SurvStacker(SurvivalAnalysisMixin):
         X_oo, y_oo = self.ss.fit_transform(X, y)
         self.times_ = self.ss.times
         self.unique_times_ = np.sort(np.unique(self.ss.times))
-        
-        # Fit classifier
-        self.clf.fit(X_oo, y_oo, **kwargs)
+
+        if self.type_sim != "none":
+            half_n = X_oo.shape[0] // 2
+            X_train_oo, X_calib_oo, y_train_oo, y_calib_oo = train_test_split(
+                X_oo, y_oo, test_size=half_n, random_state=self.random_state, 
+                stratify = y_oo)        
+            # Fit classifier
+            self.clf.fit(X_train_oo, y_train_oo, **kwargs)
+            # Calibrate classifier
+            calib_probs = self.clf.predict_proba(X_calib_oo)
+            encoder = OneHotEncoder(sparse=False)
+            test_probs = encoder.fit_transform(y_calib_oo.reshape(-1, 1))
+            self.calibrated_residuals_ = test_probs - calib_probs
+            self.clf.fit(X_calib_oo, y_calib_oo, **kwargs)
+            # Set baseline model
+            event, time = check_array_survival(X, y)
+            self._set_baseline_model(X, event, time)
+            return self
         
         # Set baseline model
+        self.clf.fit(X_oo, y_oo, **kwargs)
         event, time = check_array_survival(X, y)
         self._set_baseline_model(X, event, time)
         return self        
         
+
     def _predict_survival_function_temp(self, X):
         """
         Predict the survival function for the given input samples.
@@ -133,33 +172,77 @@ class SurvStacker(SurvivalAnalysisMixin):
         """
         X_risk, _ = self.ss.transform(X)
         oo_test_estimates = self.clf.predict_proba(X_risk)[:, 1]
-        return self.ss.predict_survival_function(oo_test_estimates)
+
+        if self.type_sim == "none":
+            # If no simulation, return the test estimates
+            return self.ss.predict_survival_function(oo_test_estimates)
+        
+        # Apply the calibrated residuals        
+        simulations_oo_test_estimates = simulate_replications(data=self.calibrated_residuals_, num_replications=self.replications, method=self.type_sim)
+        # Add the calibrated residuals to the test estimates
+        oo_test_estimates = np.tile(oo_test_estimates, (self.replications, 1)).T + simulations_oo_test_estimates
+        # clip values to be between 0 and 1
+        oo_test_estimates = np.clip(oo_test_estimates, 0, 1)
+        return [self.ss.predict_survival_function(oo_test_estimates[:, i]) for i in range(self.replications)]
+
 
     def predict(self, X, threshold=0.5):
-        surv = self._predict_survival_function_temp(X)  # shape: (n_samples, n_times)
-        crossings = surv <= threshold  # Boolean array of threshold crossings
+
+        surv = self._predict_survival_function_temp(X)  
+
+        if self.type_sim == "none":
+            crossings = surv <= threshold  # Boolean array of threshold crossings        
+            # For each sample, get the index of the first crossing (or -1 if none)
+            cross_indices = np.argmax(crossings, axis=1)        
+            # Handle cases where survival never crosses the threshold:
+            # argmax returns 0 if no True found, so we need to check if the crossing is valid
+            valid_crossings = crossings[np.arange(len(crossings)), cross_indices]        
+            # Map to actual times
+            predicted_times = np.where(
+                valid_crossings,
+                self.unique_times_[cross_indices],
+                self.unique_times_[-1]  # use max time if no crossing
+            )        
+            return predicted_times
         
-        # For each sample, get the index of the first crossing (or -1 if none)
-        cross_indices = np.argmax(crossings, axis=1)
-        
+        crossings = [s <= threshold for s in surv]
+        cross_indices = [np.argmax(cross) for cross in crossings]
         # Handle cases where survival never crosses the threshold:
         # argmax returns 0 if no True found, so we need to check if the crossing is valid
-        valid_crossings = crossings[np.arange(len(crossings)), cross_indices]
-        
+        valid_crossings = [cross[ci] for cross, ci in zip(crossings, cross_indices)]
         # Map to actual times
-        predicted_times = np.where(
-            valid_crossings,
-            self.unique_times_[cross_indices],
-            self.unique_times_[-1]  # use max time if no crossing
-        )
-        
-        return predicted_times
+        predicted_times = [
+            np.where(
+                valid,
+                self.unique_times_[ci],
+                self.unique_times_[-1]  # use max time if no crossing
+            ) for valid, ci in zip(valid_crossings, cross_indices)
+        ]
+        return np.array(predicted_times)
     
 
     def predict_cumulative_hazard_function(self, X, return_array=False):
-        return self._predict_cumulative_hazard_function(
-            self._get_baseline_model(), self.predict(X), return_array
-        )
+        """
+        Predict cumulative hazard function.
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The input samples.
+        return_array : bool, default=False
+            Whether to return the cumulative hazard function as an array.
+        Returns
+        -------
+        array-like or list of StepFunction
+            Predicted cumulative hazard function for each sample.
+        """
+        if self.type_sims == "none":
+            # Convert X to numpy array if needed
+            return self._predict_cumulative_hazard_function(
+                self._get_baseline_model(), self.predict(X), return_array)
+        return [self._predict_cumulative_hazard_function(
+            self._get_baseline_model(), s, return_array
+        ) for s in self._predict_survival_function_temp(X)]
+
 
     def predict_survival_function(self, X, return_array=False):
         """
@@ -183,13 +266,24 @@ class SurvStacker(SurvivalAnalysisMixin):
             
         # Get predictions using temporary method
         surv = self._predict_survival_function_temp(X)
+
+        if self.type_sim == "none":
         
-        if return_array:
-            return surv
-            
-        # Convert to StepFunction instances
-        funcs = []
-        for i in range(surv.shape[0]):
-            func = StepFunction(x=self.unique_times_, y=surv[i])
-            funcs.append(func)
-        return np.array(funcs)
+            if return_array:
+                return surv
+                
+            # Convert to StepFunction instances
+            funcs = []
+            for i in range(surv.shape[0]):
+                func = StepFunction(x=self.unique_times_, y=surv[i])
+                funcs.append(func)
+            return np.array(funcs)
+        
+        else:
+
+            # Convert to StepFunction instances
+            funcs = []
+            for i in range(len(surv)):
+                func = StepFunction(x=self.unique_times_, y=surv[i])
+                funcs.append(func)
+            return np.array(funcs)
